53391670ed76267af8849f3e724b51b4bc765609
Banujan Ravishankar <banujanr29@gmail.com>
2026-01-31 22:42:26 +0530

    Fix quality issues

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,15 @@
-package com.example;
+package main.java.com.example;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
+
         Calculator calc = new Calculator();
-        System.out.println(calc.calculate(10, 5, "add-again"));
+
+        System.out.println(calc.calculate(10, 5, "add"));
+
         UserService service = new UserService();
         service.findUser("admin");
-        service.deleteUser("admin"); // NEW dangerous call
     }
 }
 
